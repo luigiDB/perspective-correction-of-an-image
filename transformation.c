6d2fa1a7ce4bd804ffee02372ca4976dc1c554f7
luigiDB <spada.elfica@gmail.com>
2015-07-10 00:23:36 +0200

    Now homography works like a function

--- transformation.c
+++ transformation.c
@@ -1,32 +1,6 @@
-/*
-2nd Project Advanced Topics in Computer Systems and Network.
-I give in input these parameters: input file (edge ?) output file.
+#include "transformation.h"
 
-If the inpunt file is not specified the source code get for default the inputfile.jpg. 
 
-If the file it doesn't exist will appeare an error
-The default output file is  output.jpg.
-It will be shown.
-
-In order to compile you have to write this on the console:
-gcc transformation.c `pkg-config --cflags --libs opencv`
-
-Where transformation.c is the name of this source code.
-If it doesn't compile check if the libraries are installed.
-
-In order to execute:
-/a.out RMR5y.png ciao.jpg
-
-Where RMR5y.png is the input file and ciao.jpg is the output file.
-*/
-
-#include <opencv/highgui.h>
-//#include <opencv/cv.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-#define AUTO_SIZE_YES 1
-#define JPEG_QUALITY 80
 void homography_transformation(IplImage* src, CvPoint2D32f* srcQuad,char* out_filename){//Input image, Edges, outputNameFile
 	CvPoint2D32f dstQuad[4]; //Destination matrix
     CvMat* warp_matrix = cvCreateMat(3,3,CV_32FC1);     // transformation matrix
@@ -47,10 +21,10 @@ void homography_transformation(IplImage* src, CvPoint2D32f* srcQuad,char* out_fi
   	///Set all scalar with the same value. 0 means the black color of border
 	cvWarpPerspective(src, dst, warp_matrix, 1, cvScalarAll(0)); // 1 = CV_INTER_LINEAR
 	cvNamedWindow( out_filename, 1 );//Window name and autosize YES (1)
-int p[3]={CV_IMWRITE_JPEG_QUALITY,JPEG_QUALITY,0};//FORMAT, QUALITY, BOH
+	int p[3]={CV_IMWRITE_JPEG_QUALITY,JPEG_QUALITY,0};//FORMAT, QUALITY, BOH
 
 	cvSaveImage(out_filename,dst, p);
-//close and destroy all stuff
+	//close and destroy all stuff
 
 	cvReleaseMat(&warp_matrix);
 
@@ -58,31 +32,19 @@ int p[3]={CV_IMWRITE_JPEG_QUALITY,JPEG_QUALITY,0};//FORMAT, QUALITY, BOH
 	cvReleaseImage(&dst);
 }
 
-int main( int argc, char** argv ) {
-IplImage *dst=NULL;
-char* filename = argc >= 2 ? argv[1] : "Input.jpg";//Se il secondo argomento non è specificato, prende di default Input.jpg
-char* out_filename= argc ==3 ? argv[2] : "output.jpg"; //Se Il terzo argomento non è specificato, come nome dell'output mette output.jpg
-IplImage *src = cvLoadImage(filename,1);
-//Check on src
-if(src==NULL){
-fprintf(stderr,"%s input file not present\n",filename);
-exit(0);
-}
+int homography( char* input, char* output, CvPoint2D32f* srcQuad ) {
+	IplImage *src = cvLoadImage(input,1);
+	
+	//Check on src
+	if(input == NULL){
+		fprintf(stderr,"%s input file not present\n",filename);
+		return 1;
+	}
+	
+	if(output == NULL){
+		output = (char*)malloc(sizeof("output.jpg")+1);
+		output = 
+	}
 
-    CvPoint2D32f srcQuad[4]; //Source corners.
-//Corner of trapezoid. Arbitrary values.
-
-    srcQuad[0].x = 236;   //src Top left
-    srcQuad[0].y = 511;
-
-    srcQuad[1].x = 1336;   //src Top right
-    srcQuad[1].y = 473;
-
-    srcQuad[2].x = 1;   //src Bottom left
-    srcQuad[2].y = 1859;
-
-    srcQuad[3].x = 1507;   //src Bottom right
-    srcQuad[3].y = 1952;
-
-homography_transformation(src,srcQuad,out_filename);
+	homography_transformation(src,srcQuad,output);
 }
---
+++ transformation.h
@@ -0,0 +1,35 @@
+/*
+2nd Project Advanced Topics in Computer Systems and Network.
+I give in input these parameters: input file (edge ?) output file.
+
+If the inpunt file is not specified the source code get for default the inputfile.jpg. 
+
+If the file it doesn't exist will appeare an error
+The default output file is  output.jpg.
+It will be shown.
+
+In order to compile you have to write this on the console:
+gcc transformation.c `pkg-config --cflags --libs opencv`
+
+Where transformation.c is the name of this source code.
+If it doesn't compile check if the libraries are installed.
+
+In order to execute:
+/a.out RMR5y.png ciao.jpg
+
+Where RMR5y.png is the input file and ciao.jpg is the output file.
+*/
+
+
+#include <opencv/highgui.h>
+//#include <opencv/cv.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#define AUTO_SIZE_YES 1
+#define JPEG_QUALITY 80
+
+
+void homography_transformation(IplImage* src, CvPoint2D32f* srcQuad,char* out_filename);
+int homography( char* input, char* output, CvPoint2D32f* srcQuad );
